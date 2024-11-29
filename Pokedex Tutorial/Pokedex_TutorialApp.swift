61c4f8d0f889f860aa86e93e18ffa3b79b334fc3
Brian Corbin <brian.william.corbin@gmail.com>
2024-11-29 10:51:24 -0500

    adding project template

---
+++ Pokedex Tutorial/Pokedex_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pokedex_TutorialApp.swift
+//  Pokedex Tutorial
+//
+//  Created by Brian Corbin on 11/29/24.
+//
+
+import SwiftUI
+
+@main
+struct Pokedex_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
