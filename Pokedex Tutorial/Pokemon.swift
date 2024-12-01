6264f4fee6d6c074a7f94c34dea1e6a025503557
Brian Corbin <brian.william.corbin@gmail.com>
2024-11-30 20:12:57 -0500

    Step 2a: Adding Pokemon struct

---
+++ Pokedex Tutorial/Pokemon.swift
@@ -0,0 +1,14 @@
+//
+//  Pokemon.swift
+//  Pokedex Tutorial
+//
+//  Created by Brian Corbin on 11/30/24.
+//
+
+import SwiftUI
+
+struct Pokemon {
+    let id: Int
+    let name: String
+    let types: [PokemonType]
+}
