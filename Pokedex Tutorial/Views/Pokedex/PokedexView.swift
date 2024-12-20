aa5b977226cfd222f75a002f41f0b18e496499ae
Brian Corbin <brian.william.corbin@gmail.com>
2024-12-20 06:44:41 -0800

    Step 9a: Presenting sheet detail view

--- Pokedex Tutorial/Views/Pokedex/PokedexView.swift
+++ Pokedex Tutorial/Views/Pokedex/PokedexView.swift
@@ -11,6 +11,7 @@ struct PokedexView: View {
     let pokemon: [PokemonSummary]
     
     @State private var searchText = ""
+    @State private var selectedPokemon: PokemonSummary?
     
     var filteredPokemon: [PokemonSummary] {
         guard !searchText.isEmpty else { return pokemon }
@@ -24,6 +25,12 @@ struct PokedexView: View {
                     ForEach(filteredPokemon) { pokemon in
                         PokemonSummaryView(pokedexNumber: pokemon.id, pokemonName: pokemon.name, types: pokemon.types)
                             .padding(.horizontal, 10)
+                            .onTapGesture {
+                                selectedPokemon = pokemon
+                            }
+                            .sheet(item: $selectedPokemon) { selectedPokemon in
+                                PokemonDetailsView(pokemonSummary: selectedPokemon, pokemon: nil)
+                            }
                     }
                 }
             }
